a3e372bb37ff3f3b1a7cf671737db50435939497
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-03-25 13:44:24 -0300

    Adiciona Read

--- EPlayers_ASPNet/Models/Match.cs
+++ EPlayers_ASPNet/Models/Match.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using EPlayers_ASPNet.Interfaces;
 
 namespace EPlayers_ASPNet.Models
 {
-    public class Match
+    public class Match : EPlayersBase , IMatch
     {
         
         public int MatchId { get; set; }
@@ -10,11 +13,42 @@ namespace EPlayers_ASPNet.Models
         public int PlayerTwoId { get; set; }
         public DateTime StartHour { get; set; }
         public DateTime EndHour { get; set; }
+        const string PATH = "Database/Match.csv";
 
         public Match(){
-            StartHour = DateTime.UtcNow;
-            EndHour = StartHour.AddMinutes(30);
+            CreateDatabase(PATH);
         }
-        
+
+        public void Create(Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Match> Read()
+        {
+            List<Match> matchList = new List<Match>();
+            string[] content = File.ReadAllLines(PATH);
+            foreach (var item in content)
+            {
+                Match match = new Match();
+                match.MatchId = int.Parse(item.Split(";")[0]);
+                match.PlayerOneId = int.Parse(item.Split(";")[1]);
+                match.PlayerTwoId = int.Parse(item.Split(";")[2]);
+                match.StartHour = DateTime.Parse(item.Split(";")[3]);
+                match.EndHour = DateTime.Parse(item.Split(";")[4]);
+                matchList.Add(match);
+            }
+            return matchList;
+        }
+
+        public void Update(Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
